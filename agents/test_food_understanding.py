bd4d18af1ab8ebb169ec260551509cc979609523
Pritee Joshi <pritee814319@gmail.com>
2026-07-22 17:48:03 -0400

    Update test_food_understanding.py

--- agents/test_food_understanding.py
+++ agents/test_food_understanding.py
@@ -1,1 +1,24 @@
 
+from agents.food_understanding_agent import food_understanding_agent
+
+
+tests = [
+
+    "chicken handi",
+
+    "chicken hundi",
+
+    "butter chiken",
+
+    "PIZZA",
+
+    "sushi"
+
+]
+
+
+for food in tests:
+
+    result = food_understanding_agent(food)
+
+    print(result)
