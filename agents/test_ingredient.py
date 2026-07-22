fc1c4bbb5303b701e637ed37555d4b3b14df1cb0
Pritee Joshi <pritee814319@gmail.com>
2026-07-22 15:01:37 -0400

    Update test_ingredient.py

--- agents/test_ingredient.py
+++ agents/test_ingredient.py
@@ -1,1 +1,19 @@
+from agents.ingredient_agent import ingredient_agent
 
+
+ingredients = [
+
+    "1 kg Chicken",
+    "2 Tomatoes",
+    "1 Onion",
+    "1 cup Yogurt"
+
+]
+
+
+result = ingredient_agent(
+    ingredients
+)
+
+
+print(result)
