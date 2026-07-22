ec5fa632e0998d7000265b45d3aa4086779187b2
Pritee Joshi <pritee814319@gmail.com>
2026-07-22 15:24:52 -0400

    Update recipe_agent.py

--- agents/recipe_agent.py
+++ agents/recipe_agent.py
@@ -5,21 +5,32 @@ def recipe_agent(food):
 
     meals = search_recipes(food)
 
+
     if not meals:
+
         return {
             "error": "No recipes found"
         }
 
+
     recipes = []
+
 
     for meal in meals[:5]:
 
         ingredients = []
 
+
         for i in range(1, 21):
 
-            ingredient = meal.get(f"strIngredient{i}")
-            measure = meal.get(f"strMeasure{i}")
+            ingredient = meal.get(
+                f"strIngredient{i}"
+            )
+
+            measure = meal.get(
+                f"strMeasure{i}"
+            )
+
 
             if ingredient and ingredient.strip():
 
@@ -27,20 +38,22 @@ def recipe_agent(food):
                     f"{measure.strip()} {ingredient.strip()}"
                 )
 
+
         recipes.append({
 
-    "Recipe": meal["strMeal"],
+            "Recipe": meal["strMeal"],
 
-    "Cuisine": meal["strArea"],
+            "Cuisine": meal["strArea"],
 
-    "Category": meal["strCategory"],
+            "Category": meal["strCategory"],
 
-    "Ingredients": ingredients,
+            "Ingredients": ingredients,
 
-    "Instructions": meal["strInstructions"],
+            "Instructions": meal["strInstructions"],
 
-    "Image": meal["strMealThumb"]
+            "Image": meal["strMealThumb"]
 
-})
+        })
+
 
     return recipes
