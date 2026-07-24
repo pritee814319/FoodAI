f45397ac2dae29b1edba7836642f6b2d43c6d266
Pritee Joshi <pritee814319@gmail.com>
2026-07-24 13:41:12 -0400

    Update test_recipe_parser.py with sample recipe data

--- agents/test_recipe_parser.py
+++ agents/test_recipe_parser.py
@@ -1,15 +1,29 @@
-
 from agents.recipe_parser_agent import recipe_parser_agent
 
 
+recipe = {
 
-url = "PUT_RECIPE_URL_HERE"
+    "Recipe":
+    "Poha Recipe",
 
+    "URL":
+    "https://www.vegrecipesofindia.com/poha-recipe-poha/"
+
+}
 
 
 result = recipe_parser_agent(
-    url
+    recipe
 )
 
 
-print(result)
+print("\nINGREDIENTS")
+print(
+    result.get("Ingredients")
+)
+
+
+print("\nINSTRUCTIONS")
+print(
+    result.get("Instructions")
+)
